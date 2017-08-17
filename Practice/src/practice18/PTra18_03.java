210a6966927d428eb96787933ed043ad420d85d2
YUKI\佑樹 <yuki.h.56.spirit@gmail.com>
2017-08-17 11:21:29 +0900

    practice18終了

--- Practice/src/practice18/PTra18_03.java
+++ Practice/src/practice18/PTra18_03.java
@@ -26,7 +26,7 @@ public class PTra18_03 {
 		 * ★ file/BestElevenCandidate.csvの内容を取得し、１行毎にPlayerインスタンスに情報を格納してください
 		 * ★ ArrayListを作成して、Playerインスタンスを格納してください
 		 */
-		ArrayList<Player> array = new ArrayList<>();
+		ArrayList<Player> playerList = new ArrayList<>();
         try(Scanner scanner = new Scanner(new File("file/BestElevenCandidate.csv"))) {
             while (scanner.hasNext()) {
                 String line = scanner.nextLine();
@@ -40,7 +40,7 @@ public class PTra18_03 {
                 player.country = stringList[2];
                 player.team = stringList[3];
 
-                array.add(player);
+                playerList.add(player);
 
 
             }
@@ -51,15 +51,16 @@ public class PTra18_03 {
 
 		// ★ ①のArrayListの中から"レアル・マドリード", "バルセロナ"の選手を除外してください
 
-        for (int i = array.size()-1; i >=0 ; i--) {
-        	if(array.get(i).getTeam().equals("レアル・マドリード") || array.get(i).getTeam().equals("バルセロナ")) {
-        		System.out.println(array.get(i).getTeam());
-        		array.remove(i);
+        
+        for (int i = playerList.size()-1; i >=0 ; i--) {
+        	if(playerList.get(i).getTeam().equals("レアル・マドリード") || playerList.get(i).getTeam().equals("バルセロナ")) {
+        		System.out.println(playerList.get(i).getTeam());
+        		playerList.remove(i);
         	}
 		}
 
 		// ★ 削除後のArrayListの中身を全件出力してください
-        for(Player player : array) {
+        for(Player player : playerList) {
             System.out.println(player.toString());
 	    }
 
--- Practice/src/practice18/PTra18_04.java
+++ Practice/src/practice18/PTra18_04.java
@@ -41,17 +41,124 @@ public class PTra18_04 {
             System.out.println("ファイルが見つかりません");
         }
 
-
+        int gkCount = 0;
+        int dfCount = 0;
+        int mfCount = 0;
+        int fwCount = 0;
 
 		// ★ ①のArrayListの中からGK1名、DF4名、MF4名, FW2名をランダムで出力してください
         Collections.shuffle(playerList);
         
-        for (int i = 0; i < playerList.size(); i++) {
-			if(playerList.get(i).getPosition().equals("gk"));{
-				System.out.println(playerList.get(i).toString());
-			
+        for (int a = 0; a < playerList.size(); a++) {
+			if(playerList.get(a).getPosition().equals("GK")){
+				
+				System.out.println(playerList.get(a));
+				gkCount++;
+			if(gkCount == 1){
+				break;
+			}
+			}
 		}
+        Collections.shuffle(playerList);
+			
+		for (int b = 0; b < playerList.size(); b++) {
+			if(playerList.get(b).getPosition().equals("DF")){
+						
+				System.out.println(playerList.get(b));
+		        dfCount++;
+			if(dfCount == 4) {
+		    	break;
+		    }
+			}
+		}
+		Collections.shuffle(playerList);
+			
+		for (int c = 0; c < playerList.size(); c++) {
+			if(playerList.get(c).getPosition().equals("MF")){
+								
+				System.out.println(playerList.get(c));
+				mfCount++;
+			if(mfCount == 4) {
+				break;
+			}
+			}
+		}
+		Collections.shuffle(playerList);
+			
+		for (int d = 0; d < playerList.size(); d++) {
+			if(playerList.get(d).getPosition().equals("FW")){
+										
+				System.out.println(playerList.get(d));
+				fwCount++;
+			if(fwCount == 2) {
+				break;
+			}
+			}
+		}
+//		Collections.shuffle(playerList);
+//										
+//		for (int e = 0; e < playerList.size(); e++) {
+//	        if(playerList.get(e).getPosition().equals("DF")){
+//												
+//				System.out.println(playerList.get(e));
+//				break;
+//	        }
+//		}
+//		Collections.shuffle(playerList);
+//	        
+//		for (int f = 0; f < playerList.size(); f++) {
+//			if(playerList.get(f).getPosition().equals("MF")){
+//														
+//				System.out.println(playerList.get(f));
+//				break;
+//			}
+//		}
+//		Collections.shuffle(playerList);
+//			
+//		for (int g = 0; g < playerList.size(); g++) {
+//			if(playerList.get(g).getPosition().equals("MF")){
+//																
+//				System.out.println(playerList.get(g));
+//				break;
+//			}
+//		}
+//		Collections.shuffle(playerList);
+//			
+//		for (int h = 0; h < playerList.size(); h++) {
+//			if(playerList.get(h).getPosition().equals("MF")){
+//																		
+//				System.out.println(playerList.get(h));
+//				break;
+//			}
+//		}
+//		Collections.shuffle(playerList);
+//			
+//		for (int i = 0; i < playerList.size(); i++) {
+//			if(playerList.get(i).getPosition().equals("MF")){
+//																				
+//				System.out.println(playerList.get(i));
+//				break;
+//			}
+//		}
+//		Collections.shuffle(playerList);
+//			
+//		for (int j = 0; j < playerList.size(); j++) {
+//			if(playerList.get(j).getPosition().equals("FW")){
+//																						
+//				System.out.println(playerList.get(j));
+//				break;
+//			}
+//		}
+//		Collections.shuffle(playerList);
+//			
+//		for (int k = 0; k < playerList.size(); k++) {
+//			if(playerList.get(k).getPosition().equals("FW")){
+//																								
+//				System.out.println(playerList.get(k));
+//			    break;
+//			}
+//		
+//		}
 //            System.out.println(playerList.toString());
-	    }
-	}
+	 }
 }
